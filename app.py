f8fea7273db37db78b5102f92bdb1d52c56c20dd
Karim <Karim.rushdy1@gmail.com>
2025-09-05 10:06:53 +0300

    Cooking code

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Homepage
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Recipes page
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+# About page
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Contact page
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
